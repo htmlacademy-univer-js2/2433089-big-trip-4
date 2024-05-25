8d153376119362a1b3d4f73d6d32ef3de67d6538
Перегуда Виктор <pereguda.vitia@mail.ru>
2024-05-25 06:57:04 +0500

    6.6. Обновление века (часть 1)

--- src/presenter/trip-point-presenter.js
+++ src/presenter/trip-point-presenter.js
@@ -70,6 +70,7 @@ export default class TripPointPresenter {
 
   resetView = () => {
     if (this.#mode !== Mode.PREVIEW) {
+      this.#editingTripPointComponent.reset(this.#tripPoint);
       this.#replaceEditingPointToPreviewPoint();
     }
   };
@@ -90,6 +91,7 @@ export default class TripPointPresenter {
   #escKeyDownHandler = (evt) => {
     if (evt.key === 'Escape' || evt.key === 'Esc') {
       evt.preventDefault();
+      this.#editingTripPointComponent.reset(this.#tripPoint);
       this.resetView();
     }
   };
@@ -103,6 +105,7 @@ export default class TripPointPresenter {
   };
 
   #handlePreviewClick = () => {
+    this.#editingTripPointComponent.reset(this.#tripPoint);
     this.#replaceEditingPointToPreviewPoint();
   };
 
